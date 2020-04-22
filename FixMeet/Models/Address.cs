00fed8b0334fb91470ad1568ce00b0278dc224a6
or livni <orlivni8@gmail.com>
2020-04-22 10:53:25 +0300

    added models files

---
+++ FixMeet/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FixMeetWebSite.Models
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public string Srteet { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ FixMeet/Models/Request.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FixMeetWebSite.Models
+{
+    public class Request
+    {
+
+    }
+}
